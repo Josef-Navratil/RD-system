8708b1cd1e2c19f6c4615902396c5a5b92df9951
Josef-Navratil <pepa.navratil@gmail.com>
2018-12-31 15:07:44 +0100

    adjusted parameters in bif_point

--- find_bif_point/run_computation.py
+++ find_bif_point/run_computation.py
@@ -30,13 +30,20 @@ if __name__ == '__main__':
     BC = 'N'
     TOL = 1e-7
     source = 1.0
-    parameter_list = [[0.02, 0.17, 0.08, 0.03, 1e-6], [0.12, 0.90, 0.20, 0.02, 1e-5], [0.5, 3.80, 2.80, 0.02, 1e-4], [1.1, 8.20, 6.20, 0.01, 1e-3], [1.6, 12.90, 10.00, 0.01, 1e-3], [4.0, 33.00, 25.00, 0.01, 1e-3]]
+    parameter_list = [[0.02, 0.17, 0.08, 0.03, 1e-6], [0.12, 0.90, 0.20, 0.02, 1e-5], [0.5, 3.90, 2.80, 0.02, 1e-4], [1.1, 8.20, 6.20, 0.01, 1e-3], [1.6, 12.90, 10.00, 0.01, 1e-3], [4.0, 33.00, 25.00, 0.01, 1e-3]]
+    for [d1, d2_max, d2_min, dT, TOL_bif] in parameter_list:
+        d2_bif = find_bif_point(d1, d2_max, d2_min, BC, TOL, dT, source, TOL_bif)
+        file_values.write("d1: " + str(d1) + " d2: " + str(d2_bif) + "\n")
+        print(d2_bif)
+        
+    BC = 'D'        
+    parameter_list2 = [[0.02, 0.17, 0.08, 0.03, 1e-6], [0.12, 0.90, 0.20, 0.02, 1e-5], [0.5, 3.90, 2.80, 0.02, 1e-4], [1.1, 8.20, 6.20, 0.01, 1e-3], [1.6, 12.90, 10.00, 0.01, 1e-3], [4.0, 38.00, 27.00, 0.01, 1e-3]]
     for [d1, d2_max, d2_min, dT, TOL_bif] in parameter_list:
         d2_bif = find_bif_point(d1, d2_max, d2_min, BC, TOL, dT, source, TOL_bif)
         file_values.write("d1: " + str(d1) + " d2: " + str(d2_bif) + "\n")
         print(d2_bif)
     file_values.close()
-    #os.system('systemctl poweroff')
+    os.system('systemctl poweroff')
     
 
     
